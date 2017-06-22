4c2ee09396286305ec0afb1163c2e7de4423ebb4
anrow <602492242anrow@gmail.com>
2017-06-23 04:40:50 +0900

    files upload

---
+++ Assets/Scripts/ArtificialPosition.cs
@@ -0,0 +1,67 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ArtificialPosition : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField]
+    private int layer;
+
+    [SerializeField]
+    private Vector2 offset;
+
+    [SerializeField]
+    private Vector2 position;
+
+    #endregion Private Fields
+
+    #region Public Properties
+
+    /// <summary>
+    /// Layer the object inhabits.
+    /// </summary>
+    public int Layer
+    {
+        get { return layer; }
+        set { layer = value; }
+    }
+
+    /// <summary>
+    /// Offset of the object in contrast to world space.
+    /// </summary>
+    public Vector2 Offset
+    {
+        get { return offset; }
+        set { offset = value; }
+    }
+
+    /// <summary>
+    /// Modified position of the object.
+    /// </summary>
+    public Vector2 Position
+    {
+        get { return position; }
+        set { position = value; }
+    }
+
+    #endregion Public Properties
+
+    #region Private Methods
+
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
+    private void Update()
+    {
+        if (Application.isPlaying)
+            Destroy(this);
+        else if (Selection.activeGameObject == this.gameObject)
+        {
+            position = offset + (Vector2)transform.position;
+        }
+    }
+
+    #endregion Private Methods
+}
---
+++ Assets/Scripts/Layer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Layer : MonoBehaviour
+{
+    #region Private Fields
+
+    [SerializeField]
+    private int id;
+
+    [SerializeField]
+    private int priority;
+
+    #endregion Private Fields
+
+    #region Public Constructors
+
+    /// <summary>
+    /// Constructor for the layer.
+    /// </summary>
+    /// <param name="priority">Priority of the layer</param>
+    /// <param name="id">ID of the layer</param>
+    public Layer(int priority, int id)
+    {
+        this.id = id;
+        this.priority = priority;
+    }
+
+    #endregion Public Constructors
+
+    #region Public Properties
+
+    /// <summary>
+    /// The id of the layer.
+    /// </summary>
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    /// <summary>
+    /// The priority of the layer.
+    /// </summary>
+    public int Priority
+    {
+        get { return priority; }
+        set { priority = value; }
+    }
+
+    #endregion Public Properties
+}
